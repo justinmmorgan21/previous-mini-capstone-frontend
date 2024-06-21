dc6a418ed7c7c790fdfa74d7d5205a9928c91554
brian <riceb53@gmail.com>
2024-06-21 09:29:04 -0700

    add to cart button

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -1,7 +1,19 @@
 import axios from 'axios'
 
 export function ProductsShow(props) {
- 
+  const submit = (event) => {
+    event.preventDefault()
+    console.log('in submit')
+    const params = new FormData(event.target)
+    // pick the quantity out of event.target or add product id to the form
+    
+    // how can i make the web request the same as httpie?
+    axios.post('http://localhost:3000/carted_products.json', params).then(response => {
+      console.log(response.data)
+      // redirect to the cart
+      window.location.href = '/'
+    })
+  }
   
   return (
     <div>
@@ -10,7 +22,12 @@ export function ProductsShow(props) {
       <p>Name: {props.product.name}</p>
       <p>description: {props.product.description}</p>
       <p>price: {props.product.price}</p>      
+      <form onSubmit={submit}>
+        <p>Quantity: <input type="text" name="quantity" /></p>
+        <p><input type="hidden" name="product_id" defaultValue={props.product.id} /></p>
 
+        <input type="submit" value="add to cart" />
+      </form>
       
     </div>
   );
