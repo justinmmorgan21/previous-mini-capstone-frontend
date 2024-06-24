b92f434250c3ce65d246d47b8c130acb2fd39168
brian <riceb53@gmail.com>
2024-06-24 11:47:41 -0700

    default images and shorten name and description for styling

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -3,14 +3,16 @@ export function ProductsIndex(props) {
   return (
     <div>
       <br />
-      <div class="row">
+      <div className="row">
         {props.products.map(product => (
-          <div key={product.id} class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">{product.name}</h5>
-                <p class="card-text">{product.description}.</p>
-                <a onClick={() => props.onShowProduct(product)} class="btn btn-primary">Go somewhere</a>                
+          <div key={product.id} className="col-sm-6">
+            <div className="card">
+              <img width="200px" height="200px" src={product.images[0].url} class="card-img-top" alt="..."></img>
+              <div className="card-body">
+                <h5 className="card-title">{product.name && product.name.substring(0,30)}</h5>
+                <p className="card-text">{product.description && product.description.substring(0,8)}.</p>
+                
+                <p><a onClick={() => props.onShowProduct(product)} className="btn btn-primary">Go somewhere</a></p>
               </div>
             </div>
             <br />
@@ -21,15 +23,7 @@ export function ProductsIndex(props) {
       
       <p className="text-4xl font-bold underline">in products index</p>
       <div className="grid-cols-3 grid p-7">
-        {props.products.map(product => (
-          <div key={product.id} className="shadow-lg p-4 mb-4">
-            <p>name: {product.name}</p>
-            <p>price:  {product.price}</p>
-            {/* {JSON.stringify(product.images[0] && product.images[0].url)} */}
-            <img className="w-full aspect-[4-3] object-cover" src={product.images[0] && product.images[0].url} />
-            <button className="border rounded border-gray-500 hover:bg-gray-100" onClick={() => props.onShowProduct(product)}>More info</button>            
-          </div>
-        ))}
+        
       </div>
     </div>
   )
