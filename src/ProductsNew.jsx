081d4b9646c28107738079546cb3e0706fbd3d12
brian <riceb53@gmail.com>
2024-07-02 10:58:05 -0700

    get supplier from dropdown

--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -1,8 +1,8 @@
 import axios from "axios"
 import { useEffect, useState } from 'react'
 
-export function ProductsNew(props) {
-  const [images, setImages] = useState([''])
+export function ProductsNew(props) {  
+  const [suppliers, setSuppliers] = useState([])
   
   const createProduct = (event) => {    
     event.preventDefault();
@@ -10,6 +10,17 @@ export function ProductsNew(props) {
     props.onCreateProduct(params, () => {event.target.reset()})    
     console.log('creating product...')
   }
+
+  const getSuppliers = () => {
+    console.log('getting suppliers')
+    axios.get('/suppliers.json').then(response => {
+      console.log(response.data);
+      setSuppliers(response.data)
+
+    })
+  }
+
+  useEffect(getSuppliers, [])
 
 
   
@@ -20,6 +31,12 @@ export function ProductsNew(props) {
         <p>Name<input name="name" type="text" /></p>
         <p>Description<input name="description" type="text" /></p>
         <p>Price<input name="price" type="text" /></p>        
+        <select name="supplier_id">
+          {suppliers.map(supplier => (
+            <option value={supplier.id}>{supplier.name}</option>
+          ))}                   
+        </select>       
+        <br />
         <button>New product</button>
       </form>
     </div>
